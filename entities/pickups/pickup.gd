7c22354280717ddeef0ca6a1eb54ae6668edd0c4
KyleN <kyle@neubarth.org>
2026-03-09 22:09:11 -0600

    Lootbox globals, pickup working.

--- entities/pickups/pickup.gd
+++ entities/pickups/pickup.gd
@@ -2,13 +2,12 @@ extends Area2D
 class_name Pickup
 
 func _ready() -> void:
-	# Pickups should only react to player bodies.
 	if not body_entered.is_connected(_on_body_entered):
 		body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body: Node) -> void:
 	if body is Player:
-		apply_pickup()
+		apply_pickup(body)
 
-func apply_pickup() -> void:
-	push_warning("Collision Detected")
+func apply_pickup(player : Player) -> void:
+		push_warning("pickup.apply_pickup() called on base class.")
