eb7ff1a9642e67119f02114765faadd20d545486
KyleN <kyle@neubarth.org>
2026-03-31 21:24:40 -0600

    Hard code resources for web build

--- globals/item_globals.gd
+++ globals/item_globals.gd
@@ -5,6 +5,17 @@ const ITEM_RESOURCE_DIR := "res://resources/items"
 
 var items: Dictionary[StringName, ItemData] = {}
 
+var item_paths: Array[String]
+
+var item0 = preload("res://resources/items/item_gold_coin.tres")
+var item1 = preload("res://resources/items/item_lootbox_chaos.tres")
+var item2 = preload("res://resources/items/item_lootbox_elemental.tres")
+var item3 = preload("res://resources/items/item_lootbox_forest.tres")
+var item4 = preload("res://resources/items/item_lootbox_greed.tres")
+var item5 = preload("res://resources/items/item_lootbox_soul.tres")
+var item6 = preload("res://resources/items/item_sapling.tres")
+
+
 
 func _ready() -> void:
 	reload_items()
@@ -12,27 +23,36 @@ func _ready() -> void:
 func reload_items():
 	items.clear()
 
-	var dir := DirAccess.open(ITEM_RESOURCE_DIR)
-	if dir == null:
-		push_error("Failed to open items directory: %s" % ITEM_RESOURCE_DIR)
-		return
+	items[item0.id] = item0
+	items[item1.id] = item1
+	items[item2.id] = item2
+	items[item3.id] = item3
+	items[item4.id] = item4
+	items[item5.id] = item5
+	items[item6.id] = item6
 
-	dir.list_dir_begin()
-	var file_name := dir.get_next()
 
-	while file_name != "":
-		if not dir.current_is_dir():
-			if file_name.ends_with(".tres") or file_name.ends_with(".res"):
-				var path := ITEM_RESOURCE_DIR.path_join(file_name)
-				var resource: ItemData = load(path)
+	# var dir := DirAccess.open(ITEM_RESOURCE_DIR)
+	# if dir == null:
+	# 	push_error("Failed to open items directory: %s" % ITEM_RESOURCE_DIR)
+	# 	return
 
-				if resource:
-					items[resource.id] = resource
-				else:
-					push_error("Failed to load item: %s" % path)
+	# dir.list_dir_begin()
+	# var file_name := dir.get_next()
 
-		file_name = dir.get_next()
+	# while file_name != "":
+	# 	if not dir.current_is_dir():
+	# 		if file_name.ends_with(".tres") or file_name.ends_with(".res"):
+	# 			var path := ITEM_RESOURCE_DIR.path_join(file_name)
+	# 			var resource: ItemData = load(path)
 
-	dir.list_dir_end()
+	# 			if resource:
+	# 				items[resource.id] = resource
+	# 			else:
+	# 				push_error("Failed to load item: %s" % path)
+
+	# 	file_name = dir.get_next()
+
+	# dir.list_dir_end()
 
 	print("Loaded %d items." % items.size())
--- globals/lootbox_globals.gd
+++ globals/lootbox_globals.gd
@@ -5,6 +5,11 @@ const LOOTBOX_RESOURCE_DIR := "res://resources/lootboxes"
 
 var lootboxes: Dictionary[StringName, Lootbox] = {}
 
+var item0 = preload("res://resources/lootboxes/lootbox_chaos.tres")
+var item1 = preload("res://resources/lootboxes/lootbox_elemental.tres")
+var item2 = preload("res://resources/lootboxes/lootbox_forest.tres")
+var item3 = preload("res://resources/lootboxes/lootbox_greed.tres")
+var item4 = preload("res://resources/lootboxes/lootbox_soul.tres")
 
 func _ready() -> void:
 	reload_lootboxes()
@@ -12,27 +17,33 @@ func _ready() -> void:
 func reload_lootboxes():
 	lootboxes.clear()
 
-	var dir := DirAccess.open(LOOTBOX_RESOURCE_DIR)
-	if dir == null:
-		push_error("Failed to open lootboxes directory: %s" % LOOTBOX_RESOURCE_DIR)
-		return
+	lootboxes[item0.id] = item0
+	lootboxes[item1.id] = item1
+	lootboxes[item2.id] = item2
+	lootboxes[item3.id] = item3
+	lootboxes[item4.id] = item4
 
-	dir.list_dir_begin()
-	var file_name := dir.get_next()
+	# var dir := DirAccess.open(LOOTBOX_RESOURCE_DIR)
+	# if dir == null:
+	# 	push_error("Failed to open lootboxes directory: %s" % LOOTBOX_RESOURCE_DIR)
+	# 	return
 
-	while file_name != "":
-		if not dir.current_is_dir():
-			if file_name.ends_with(".tres") or file_name.ends_with(".res"):
-				var path := LOOTBOX_RESOURCE_DIR.path_join(file_name)
-				var resource: Lootbox = load(path)
+	# dir.list_dir_begin()
+	# var file_name := dir.get_next()
 
-				if resource:
-					lootboxes[resource.id] = resource
-				else:
-					push_error("Failed to load lootbox: %s" % path)
+	# while file_name != "":
+	# 	if not dir.current_is_dir():
+	# 		if file_name.ends_with(".tres") or file_name.ends_with(".res"):
+	# 			var path := LOOTBOX_RESOURCE_DIR.path_join(file_name)
+	# 			var resource: Lootbox = load(path)
 
-		file_name = dir.get_next()
+	# 			if resource:
+	# 				lootboxes[resource.id] = resource
+	# 			else:
+	# 				push_error("Failed to load lootbox: %s" % path)
 
-	dir.list_dir_end()
+	# 	file_name = dir.get_next()
+
+	# dir.list_dir_end()
 
 	print("Loaded %d lootboxes." % lootboxes.size())
